b31bdbf788950e5cbca619feed0db80f57aa9747
Jer <jerwin.vardeleon@gmail.com>
2026-02-20 17:45:19 +0800

    0220

--- script.js
+++ script.js
@@ -2,8 +2,31 @@ const products = [
   { id: 1, name: "Keychain (bottle)", price: 35, category: "keychain", bimg: "img/products/KC_bottle.jpg" },
   { id: 2, name: "Keychain (acrylic)", price: 35, category: "keychain" , bimg: "img/products/KC_acrylic.jpg"},
   { id: 3, name: "Pin (badge)", price: 35, category: "pins", bimg: "img/products/pin.jpg" },
-  { id: 4, name: "Baptismal candle", price: 40, category: "candle" , bimg: "img/products/bapt.jpg"},
-  { id: 5, name: "Souvenir Mug", price: 10, category: "Home" }
+  { id: 4, name: "Baptismal candle", price: 40, category: "candle", bimg: "img/products/bapt.jpg" },
+  { id: 5, name: "Card type (alcohol)", price: 45, category: "alcohol", bimg: "img/products/card_alc.jpg" },
+  { id: 6, name: "Spray Bottle (alcohol)", price: 40, category: "alcohol", bimg: "img/products/spry_alc.png" },
+  { id: 7, name: "Spray Bottle (perfume)", price: 65, category: "perfume", bimg: "img/products/spry_per.png" },
+  { id: 8, name: "Keychain (bear)", price: 50, category: "keychain", bimg: "img/products/spry_per.png" },
+  { id: 9, name: "Omni Hand Sanitizer", price: 55, category: "sanitizer", bimg: "img/products/sani.jpg" },
+  { id: 10, name: "Omni Hand Sanitizer (Organza Pouch)", price: 65, category: "sanitizer", bimg: "img/products/sani_org.jpg" },
+  { id: 11, name: "Omni Hand Sanitizer (Burlap Pouch)", price: 70, category: "sanitizer", bimg: "img/products/sani_bur.jpg" },
+  { id: 12, name: "Wet wipes in can", price: 65, category: "keychain", bimg: "img/products/wipe.jpg" },
+  { id: 13, name: "Bear (pouch)", price: 80, category: "bear", bimg: "img/products/bear.png" },
+  { id: 14, name: "Bear (bottle)", price: 100, category: "bear", bimg: "img/products/bearb.jpg" },
+  { id: 15, name: "Bear w/ Sanitizer", price: 150, category: "bear", bimg: "img/products/bearh.jpg" },
+  { id: 16, name: "Bear w/ Mug", price: 185, category: "bear", bimg: "img/products/bearm.jpg" },
+  { id: 17, name: "Safari Plush Toy", price: 100, category: "bear", bimg: "img/products/safari.jpg" },
+  { id: 17, name: "Safari Plush Toy (pouch)", price: 120, category: "bear", bimg: "img/products/safarip.png" },
+  { id: 18, name: "Safari Plush Toy (acetate box)", price: 150, category: "bear", bimg: "img/products/safaria.png" },
+  { id: 19, name: "Safari Plush Toy w/ Mug", price: 180, category: "bear", bimg: "img/products/safarim.png" },
+  { id: 20, name: "Mini Scented bubble candle", price: 50, category: "candle", bimg: "img/products/cand.jpeg" },
+  { id: 21, name: "Spray Bottle - Gold (perfume)", price: 55, category: "perfume", bimg: "img/products/perfg.jpg" },
+  { id: 22, name: "Scented Candle", price: 65, category: "candle", bimg: "img/products/scncand.jpeg" },
+  { id: 22, name: "Keychain (perfume)", price: 55, category: "keychain", bimg: "img/products/KC_ess.png" },
+
+  { id: 71, name: "Spray Bottle (perfume)", price: 65, category: "spray bottle", bimg: "img/products/spry_per.png" },
+  { id: 61, name: "Spray Bottle (alcohol)", price: 40, category: "spray bottle", bimg: "img/products/spry_alc.png" },
+  { id: 211, name: "Spray Bottle - Gold (perfume)", price: 55, category: "spray bottle", bimg: "img/products/perfg.jpg" }
 ];
 
 let cart = [];
@@ -47,7 +70,8 @@ function renderProducts() {
     card.className = "card";
 
     card.innerHTML = `
-      <div style="height:246px;background-image: url(${product.bimg});background-size: contain;background-repeat: no-repeat;border-radius:15px;"></div>
+      <div style="height:246px;background-image: url(${product.bimg});background-size: cover;background-repeat: no-repeat;border-radius:15px; max-width=246px" ; >
+      </div>
       <h3>${product.name}</h3>
       <p>${product.category}</p>
       <strong>₱ ${product.price}</strong><br><br>
